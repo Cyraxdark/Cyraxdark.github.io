e8d92efafd7cc05a283bb241616271e865567352
Cyraxdark <darknesvoid59@gmail.com>
2022-06-17 11:58:15 -0500

    version1.1

---
+++ numero.js
@@ -0,0 +1,7 @@
+let numero;
+numero = promt("Introduce algun numero")
+if(numero / 2 == 0){
+document.writer(" El numero "+ numero + " es Par ");
+}else{
+document.write(" El numero " + numero + " es Impar ");
+}
